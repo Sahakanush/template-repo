b435cf5ef82651d887269acd98b18c294281c7ae
Sahakanush <95763931+Sahakanush@users.noreply.github.com>
2023-02-05 12:07:55 +0400

    Update change_tag.py

--- .github/change_tag.py
+++ .github/change_tag.py
@@ -3,9 +3,9 @@ import sys
 
 image = sys.argv[1]
 
-with open('../../ArgoApps/app.yaml') as file:
+with open('../ArgoApps/app.yaml') as file:
   data_dict = yaml.load(file)
   data_dict['spec']['template']['spec']['containers'][0]['image'] = image
   
-  with open('../../ArgoApps/app.yaml', 'w') as f:
+  with open('../ArgoApps/app.yaml', 'w') as f:
         yaml.dump(data_dict, f)
